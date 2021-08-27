c4b735bbf69eb361f553fe9d460dfa83caee2bdf
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-26 23:35:33 -0400

    Add Cellar land restrictions.

--- landlock.cpp
+++ landlock.cpp
@@ -271,6 +271,7 @@ EX bool incompatible1(eLand l1, eLand l2) {
   if(l1 == laPrairie && l2 == laCrossroads4) return true;
   if(l1 == laWet && l2 == laDesert) return true;
   if(l1 == laFrog && l2 == laMotion) return true;
+  if(l1 == laCellar && !(isCrossroads(l2) || l2 == laPalace)) return true;
   if(isElemental(l1) && isElemental(l2)) return true;
   return false;
   }
